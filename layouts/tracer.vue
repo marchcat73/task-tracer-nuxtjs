3abe118def8a006bf86c0a6b9ed33ddecda804fe
unknown <D:\Program Files (x86)\The Bat!\Mail>
2019-06-10 08:34:07 +0400

    commit

---
+++ layouts/tracer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tracer-wrap">
+    <el-container class="tracer-container">
+      <el-aside width="200px">
+        <app-aside />
+      </el-aside>
+      <el-main>
+        <nuxt />
+      </el-main>
+    </el-container>   
+  </div>
+</template>
+
+<script>
+import AppAside from '@/components/tracer/Aside'
+export default {
+  components: {
+    AppAside
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .tracer-wrap {
+    width: 100%;
+    height: 100vh;
+  }
+  .tracer-container {
+    height: 100%;
+  }
+</style>
+
+
---
+++ pages/tracer/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Добро пожаловать в планировщик задач</h1>
+</template>
+
+<script>
+export default {
+  layout: 'tracer'
+}
+</script>
+
