c95df2d76cb41feb388024fb2e5e315ad656b1d5
Vitaliy Vanyushin <v.vanyushin@pro100agency.ru>
2019-06-17 17:59:11 +0400

    commit

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -10,11 +10,12 @@ module.exports.login = async (req, res) => {
     const isPasswordCorrect = bcrypt.compareSync(req.body.password, candidate.password)
 
     if (isPasswordCorrect) {
+	  const userId = candidate._id
       const token = jwt.sign({
         email: candidate.email,
         userId: candidate._id
       }, keys.JWT, {expiresIn: 60 *60})
-      res.json({token})
+      res.json({token, userId})
     } else {
       res.status(401).json({message: 'Пароль неверен'})
     }
--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -1,9 +1,38 @@
 const passport = require('passport')
 const {Router} = require('express')
+const ctr = require('../controllers/task.controller')
 const router = Router()
 
 // /api/task
 
+router.post(
+  '/', 
+  passport.authenticate('jwt', {session: false}),
+  ctr.create
+)
 
+router.get(
+  '/', 
+  passport.authenticate('jwt', {session: false}),
+  ctr.getAll 
+)
+
+router.get(
+  '/:id', 
+  passport.authenticate('jwt', {session: false}),
+  ctr.getById
+)
+
+router.put(
+  '/:id', 
+  passport.authenticate('jwt', {session: false}),
+  ctr.update
+)
+
+router.delete(
+  '/:id', 
+  passport.authenticate('jwt', {session: false}),
+  ctr.remove
+)
 
 module.exports = router
