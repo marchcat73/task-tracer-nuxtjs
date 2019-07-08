648eaeb30059830b62c5377bbf3b06776a6c29ee
unknown <D:\Program Files (x86)\The Bat!\Mail>
2019-07-08 21:00:56 +0400

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ module.exports = {
 
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: process.env.BASE_URL || 'http://localhost:3000'
+    baseURL: process.env.BASE_URL || 'http://prest.codehtml.ru:3000'
   },
 
   env: {
